ceda856387d614e16e7f2f280a0805ea3e37d401
Arseniy Yuzhanin <uzaninarsenij28@gmail.com>
2024-05-10 17:36:13 +0300

    static version (without "form")

--- scripts.js
+++ scripts.js
@@ -8,8 +8,18 @@ document.body.querySelector('[class="adding-button"]')
     .addEventListener('click', () => {
         const dialog = document.body.querySelector('dialog');
         dialog.setAttribute('style', 
-        `display: flex;
-        flex-direction: column;`);
+        `display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: 40px 30px 30px 0.5fr 1fr 0.25fr;
+        row-gap: 10px;
+        column-gap: 10px;
+        grid-template-areas:
+            "header header header"
+            "name name name"
+            "selector selector selector"
+            "first-explanatory-note second-explanatory-note third-explanatory-note"
+            "first-download-block second-download-block description-block"
+            ". done-button .";`);
         dialog.showModal();
     });
 
