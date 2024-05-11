26a90ab6d73fbf3155adefba3e3da2f16568853e
Arseniy Yuzhanin <uzaninarsenij28@gmail.com>
2024-05-11 18:54:58 +0300

    done my dialog-window (almost)

--- scripts.js
+++ scripts.js
@@ -4,10 +4,28 @@ function handleCloseButton() {
     dialog.close();
 }
 
-document.body.querySelector('[class="adding-button"]')
-    .addEventListener('click', () => {
-        const dialog = document.body.querySelector('dialog');
-        dialog.setAttribute('style', 
+function changeGridLayout() {
+    const form = document.body.querySelector('form');
+    if (window.innerWidth <= 450){
+        form.setAttribute('style',
+        `display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 40px 30px 30px 0.5fr 1fr 0.25fr;
+        row-gap: 10px;
+        column-gap: 10px;
+        grid-template-areas:
+            "header"
+            "name"
+            "selector"
+            "first-explanatory-note"
+            "first-download-block"
+            "second-explanatory-note"
+            "second-download-block"
+            "third-explanatory-note"
+            "description-block"
+            "done-button";`);
+    } else {
+        form.setAttribute('style',
         `display: grid;
         grid-template-columns: repeat(3, 1fr);
         grid-template-rows: 40px 30px 30px 0.5fr 1fr 0.25fr;
@@ -20,8 +38,13 @@ document.body.querySelector('[class="adding-button"]')
             "first-explanatory-note second-explanatory-note third-explanatory-note"
             "first-download-block second-download-block description-block"
             ". done-button .";`);
+    }
+    const dialog = document.body.querySelector('dialog');
         dialog.showModal();
-    });
+}
+
+document.body.querySelector('[class="adding-button"]')
+    .addEventListener('click', () => changeGridLayout());
 
 document.querySelector('[class="close-window-adding-exercise"]')
     .addEventListener('click', handleCloseButton);
@@ -29,4 +52,4 @@ document.querySelector('[class="close-window-adding-exercise"]')
 document.querySelector('[class="done-button"]')
     .addEventListener('click', handleCloseButton);
 
-
+window.addEventListener('resize', () => changeGridLayout());
