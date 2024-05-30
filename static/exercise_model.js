6ee0bbb83dc262c4fe0a200ae81dcc56c75fff3d
Arseniy Yuzhanin <uzaninarsenij28@gmail.com>
2024-05-30 18:02:40 +0300

    add my task

--- static/exercise_model.js
+++ static/exercise_model.js
@@ -69,8 +69,20 @@ document.getElementById("goToMain").addEventListener("click", () => {
     window.location.href = "/";
 });
 
-document.getElementById("restart").addEventListener("click", () => {
+const yesButton = document.querySelector('.reboot-confirmation-dialog-button-group .yes-button');
+yesButton.addEventListener('click', () => {
     window.location.href = "workout";
+})
+
+const noButton = document.querySelector('.reboot-confirmation-dialog-button-group .no-button');
+noButton.addEventListener('click', () => {
+    const dialog = document.querySelector('.reboot-confirmation-dialog');
+    dialog.close();
+})
+
+document.getElementById("restart").addEventListener("click", () => {
+    const dialog = document.querySelector('.reboot-confirmation-dialog');
+    dialog.showModal();
 });
 
 const savingSerieButton = document.querySelector('.saveSerie');
